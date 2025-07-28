00a5361ec61c82d00a9248880c7b49645cdabd51
zAvieRZ65026 <stain-groom0t@icloud.com>
2025-07-28 11:54:33 +0200

    Correct a calculation error in the file

---
+++ rvYnG/skill-enhancement_m1rw_696470.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.kotlin) apply false
+    alias(libs.plugins.multiplatform) apply false
+    alias(libs.plugins.jvm) apply false
+    alias(libs.plugins.nativeCocoapod) apply false
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.spotless)
+    id("dev.iurysouza.modulegraph") version "0.12.0"
+    alias(libs.plugins.compose.compiler) apply false
+}
+
+moduleGraphConfig {
+    readmePath.set("./README.md")
+    heading = "### Module Graph"
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            licenseHeaderFile(
+                rootProject.file("${project.rootDir}/spotless/copyright.kt"),
+                "^(package|object|import|interface)",
+            )
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+        format("kts") {
+            target("**/*.kts")
+            targetExclude("$buildDir/**/*.kts")
+            licenseHeaderFile(rootProject.file("spotless/copyright.kt"), "(^(?![\\/ ]\\*).*$)")
+        }
+        format("misc") {
+            target("**/*.md", "**/.gitignore")
+            trimTrailingWhitespace()
+            indentWithTabs()
+            endWithNewline()
+        }
+    }
+}
