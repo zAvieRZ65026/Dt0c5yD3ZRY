ba4c604a2a624c14c12c6009d7f943db2c470e6b
zAvieRZ65026 <stain-groom0t@icloud.com>
2025-08-03 15:06:14 +0200

    Setup JS 数据模型设计

---
+++ dataModel_0803_2106_glz.js
@@ -0,0 +1,74 @@
+// 代码生成时间: 2025-08-03 21:06:13
+// Import necessary modules
+const { defineNuxtModule } = require('@nuxt/kit')
+
+// Define the data model module
+export default defineNuxtModule({
+  meta: {
+    name: 'data-model',
+    compatibility: {
+      // Compatibility with Nuxt versions
+      nuxt: '^3.0.0'
+    },
+    hooks: {
+      // Hooks to be executed
+      'nuxt:modules:done': (moduleContainer) => {
+        // Logic to execute after all modules are done
+      }
+    }
+  },
+  defaults: {
+    // Default configuration
+    dataModel: {
+      users: [],
+      posts: [],
+      comments: []
+    }
+  },
+  setup: (options, nuxtApp) => {
+    // Module setup logic
+    nuxtApp.provide('dataModel', options.dataModel)
+  }
+})
+
+// Data model definitions
+// User
+function user() {
+  // User data structure
+  return {
+    id: null,
+    name: '',
+    email: '',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+}
+
+// Post
+function post() {
+  // Post data structure
+  return {
+    id: null,
+    title: '',
+    content: '',
+    authorId: null,
+    publishedAt: new Date(),
+    updatedAt: new Date()
+  }
+}
+
+// Comment
+function comment() {
+  // Comment data structure
+  return {
+    id: null,
+    content: '',
+    postId: null,
+    authorId: null,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+}
+
+// Export data model functions
+export { user, post, comment }
